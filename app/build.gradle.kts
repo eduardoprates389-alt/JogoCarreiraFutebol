d3578705e35724a30d3e0193eba6912efcb73001
eduardoprates389-alt <eduardoprates389@gmail.com>
2026-09-22 01:30:18 -0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.jogocarreira.futebol"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.jogocarreira.futebol"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-ktx:1.10.0")
+}
