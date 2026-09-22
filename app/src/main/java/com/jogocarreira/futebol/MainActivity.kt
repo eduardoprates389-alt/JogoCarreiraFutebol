d40fcc616bbb95bfd6dd211bc32061abb7580df7
eduardoprates389-alt <eduardoprates389@gmail.com>
2026-09-22 11:36:54 -0300

    Update MainActivity.kt

--- app/src/main/java/com/jogocarreira/futebol/MainActivity.kt
+++ app/src/main/java/com/jogocarreira/futebol/MainActivity.kt
@@ -3,7 +3,14 @@ package com.jogocarreira.futebol
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
 
@@ -11,7 +18,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text("Jogo Carreira Futebol")
+            TelaInicial()
+        }
+    }
+}
+
+@Composable
+fun TelaInicial() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("JOGO CARREIRA FUTEBOL")
+
+        Button(
+            onClick = {
+                // Próxima etapa
+            }
+        ) {
+            Text("COMEÇAR CARREIRA")
         }
     }
 }
